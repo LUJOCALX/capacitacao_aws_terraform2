ddfc791bad5f7e836c24130fda8545aed84766b1
Lucio Jose Cabianca <lucio.cabianca@gmail.com>
2022-03-28 21:44:11 -0300

    atualizações

--- main.tf
+++ main.tf
@@ -2,97 +2,50 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "ljc-ec2-az-a" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t3.micro"
-  key_name               = "curso_criacao_ambiente_terraform" # key chave publica cadastrada na AWS 
-  subnet_id              = aws_subnet.ljc_subnet_a.id         # vincula a subnet direto e gera o IP automático
-  private_ip             = "172.16.10.100"
-  vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
-  user_data              = file("./userdata/nginx_a_3200.sh")
+# resource "aws_instance" "ljc-ec2-az-a" {
+#   ami                    = data.aws_ami.ubuntu.id
+#   instance_type          = "t3.micro"
+#   key_name               = "curso_criacao_ambiente_terraform" # key chave publica cadastrada na AWS 
+#   subnet_id              = aws_subnet.ljc_subnet_a.id         # vincula a subnet direto e gera o IP automático
+#   private_ip             = "172.16.10.100"
+#   vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
+#   user_data              = file("./userdata/nginx_a_3200.sh")
 
-  #<<EOT
-  # #cloud-config
-  # # update apt on boot
-  # package_update: true
-  # # install nginx
-  # packages:
-  # - nginx
-  # write_files:
-  # - content: |
-  #     <!DOCTYPE html>
-  #     <html>
-  #     <head>
-  #       <title>StackPath - Amazon Web Services Instance</title>
-  #       <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-  #       <style>
-  #         html, body {
-  #           background: #000;
-  #           height: 100%;
-  #           width: 100%;
-  #           padding: 0;
-  #           margin: 0;
-  #           display: flex;
-  #           justify-content: center;
-  #           align-items: center;
-  #           flex-flow: column;
-  #         }
-  #         img { width: 250px; }
-  #         svg { padding: 0 40px; }
-  #         p {
-  #           color: #fff;
-  #           font-family: 'Courier New', Courier, monospace;
-  #           text-align: center;
-  #           padding: 10px 30px;
-  #         }
-  #       </style>
-  #     </head>
-  #     <body>
-  #       <img src="https://www.stackpath.com/content/images/logo-and-branding/stackpath-logo-standard-screen.svg">
-  #       <p>This request was proxied from <strong>Amazon Web Services</strong></p>
-  #     </body>
-  #     </html>
-  #   path: /usr/share/app/index.html
-  #   permissions: '0644'
-  # runcmd:
-  # - cp /usr/share/app/index.html /usr/share/nginx/html/index.html
-  # EOT
+#   tags = {
+#     Name         = "Instancia AZ a"
+#     "Criado por" = "Terraform"
+#   }
+# }
 
-  tags = {
-    Name         = "Instancia AZ a"
-    "Criado por" = "Terraform"
-  }
-}
+# resource "aws_instance" "ljc-ec2-az-b" {
+#   ami                    = data.aws_ami.ubuntu.id
+#   instance_type          = "t3.micro"
+#   key_name               = "curso_criacao_ambiente_terraform" # key chave publica cadastrada na AWS 
+#   subnet_id              = aws_subnet.ljc_subnet_b.id         # vincula a subnet direto e gera o IP automático
+#   private_ip             = "172.16.20.100"
+#   vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
+#   user_data              = file("./userdata/nginx_b_3300.sh")
 
-resource "aws_instance" "ljc-ec2-az-b" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t3.micro"
-  key_name               = "curso_criacao_ambiente_terraform" # key chave publica cadastrada na AWS 
-  subnet_id              = aws_subnet.ljc_subnet_b.id         # vincula a subnet direto e gera o IP automático
-  private_ip             = "172.16.20.100"
-  vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
-  user_data              = file("./userdata/nginx_b_3300.sh")
+#   tags = {
+#     Name         = "Instancia AZ b"
+#     "Criado por" = "Terraform"
+#   }
+# }
 
-  tags = {
-    Name         = "Instancia AZ b"
-    "Criado por" = "Terraform"
-  }
-}
+# resource "aws_instance" "ljc-ec2-az-c" {
+#   ami                    = data.aws_ami.ubuntu.id
+#   instance_type          = "t3.micro"
+#   key_name               = "curso_criacao_ambiente_terraform" # key chave publica cadastrada na AWS 
+#   subnet_id              = aws_subnet.ljc_subnet_c.id         # vincula a subnet direto e gera o IP automático
+#   private_ip             = "172.16.30.100"
+#   vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
+#   user_data              = file("./userdata/nginx_c_3400.sh")
 
-resource "aws_instance" "ljc-ec2-az-c" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t3.micro"
-  key_name               = "curso_criacao_ambiente_terraform" # key chave publica cadastrada na AWS 
-  subnet_id              = aws_subnet.ljc_subnet_c.id         # vincula a subnet direto e gera o IP automático
-  private_ip             = "172.16.30.100"
-  vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
-  user_data              = file("./userdata/nginx_c_3400.sh")
-
-  tags = {
-    Name         = "Instancia AZ c"
-    "Criado por" = "Terraform"
-  }
-}
+#   tags = {
+#     Name         = "Instancia AZ c"
+#     "Criado por" = "Terraform"
+#   }
+# }
 
 
 terraform {
@@ -105,20 +58,39 @@ terraform {
 
 
 
-output "aws_instance_e_ssh" {
-  value = [
-    aws_instance.ljc-ec2-az-a.public_ip,
-    "ssh ubuntu@${aws_instance.ljc-ec2-az-a.public_dns}",
-    "NGINX_AZA_3200 http://${aws_instance.ljc-ec2-az-a.public_dns}:3200",
-    aws_instance.ljc-ec2-az-b.public_ip,
-    "ssh ubuntu@${aws_instance.ljc-ec2-az-b.public_dns}",
-    "NGINX_AZB_3300 http://${aws_instance.ljc-ec2-az-b.public_dns}:3300",
-    aws_instance.ljc-ec2-az-c.public_ip,
-    "ssh ubuntu@${aws_instance.ljc-ec2-az-c.public_dns}",
-    "NGINX_AZC_3400 http://${aws_instance.ljc-ec2-az-c.public_dns}:3400"
-  ]
-}
+# output "aws_instance_e_ssh" {
+#   value = [
+#     aws_instance.ljc-ec2-az-a.public_ip,
+#     "ssh ubuntu@${aws_instance.ljc-ec2-az-a.public_dns}",
+#     "NGINX_AZA_3200 http://${aws_instance.ljc-ec2-az-a.public_dns}:3200",
+#     aws_instance.ljc-ec2-az-b.public_ip,
+#     "ssh ubuntu@${aws_instance.ljc-ec2-az-b.public_dns}",
+#     "NGINX_AZB_3300 http://${aws_instance.ljc-ec2-az-b.public_dns}:3300",
+#     aws_instance.ljc-ec2-az-c.public_ip,
+#     "ssh ubuntu@${aws_instance.ljc-ec2-az-c.public_dns}",
+#     "NGINX_AZC_3400 http://${aws_instance.ljc-ec2-az-c.public_dns}:3400"
+#   ]
+# }
 
-output "Meu_IP" {
-  value = ["${chomp(data.http.myip.body)}"]
-}
+# output "Meu_IP" {
+#   value = ["${chomp(data.http.myip.body)}"]
+# }
+
+# module "vpc" {
+#   source            = "terraform-aws-modules/vpc/aws" # inclusive pode colocar url do git aqui
+
+#   name              = "meu_vpc_nat_gatway"
+#   cidr              = "10.0.0.0/16" # uma classe de IP
+
+#   azs               = ["us-east-1a", "us-east-1b", "us-east-1c"]
+#   private_subnets   = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+#   public_subnets    = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+#   enable_nat_gateway = true
+#   enable_vpn_gateway = true
+
+#   tags = {
+#     Terraform = true
+#     Environment = "Dev"
+#   }
+# }
--- vpc.tf
+++ vpc.tf
@@ -1,46 +1,43 @@
 resource "aws_vpc" "ljc_vpc_tf" {
-  cidr_block           = "172.16.0.0/16"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
   tags = {
     Name = "ljc-vpc-tf"
   }
 }
 
-resource "aws_subnet" "ljc_subnet_a" {
-  vpc_id                  = aws_vpc.ljc_vpc_tf.id
-  cidr_block              = "172.16.10.0/24"
-  availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "ljc-subnet-tf_a"
+# Criação das Subnets publicas nas zonas a, b e c
+resource "aws_subnet" "subnet_publica" {
+  for_each = {
+    "ljc_subnet_pub_a" : ["10.0.101.0/24", "${var.aws_region}a", "ljc_subnet_pub_a"]
+    "ljc_subnet_pub_b" : ["10.0.102.0/24", "${var.aws_region}b", "ljc_subnet_pub_b"]
+    "ljc_subnet_pub_c" : ["10.0.103.0/24", "${var.aws_region}c", "ljc_subnet_pub_c"]
   }
-}
 
-resource "aws_subnet" "ljc_subnet_b" {
-  vpc_id                  = aws_vpc.ljc_vpc_tf.id
-  cidr_block              = "172.16.20.0/24"
-  availability_zone       = "us-east-1b"
+  vpc_id            = aws_vpc.ljc_vpc_tf.id
+  cidr_block        = each.value[0]
+  availability_zone = each.value[1]
   map_public_ip_on_launch = true
-
-  tags = {
-    Name = "ljc-subnet-tf_b"
-  }
-}
-
-resource "aws_subnet" "ljc_subnet_c" {
-  vpc_id                  = aws_vpc.ljc_vpc_tf.id
-  cidr_block              = "172.16.30.0/24"
-  availability_zone       = "us-east-1c"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "ljc-subnet-tf_c"
-  }
+  tags              = merge(local.common_tags, { Name = each.value[2] })
 }
 
 
-resource "aws_internet_gateway" "gw" {
+# Criação das Subnets privadas nas zonas a, b e c
+resource "aws_subnet" "subnet_privada" {
+  for_each = {
+    "ljc_subnet_prv_a" : ["10.0.1.0/24", "${var.aws_region}a", "ljc_subnet_prv_a"]
+    "ljc_subnet_prv_b" : ["10.0.2.0/24", "${var.aws_region}b", "ljc_subnet_prv_b"]
+    "ljc_subnet_prv_c" : ["10.0.3.0/24", "${var.aws_region}c", "ljc_subnet_prv_c"]
+  }
+  vpc_id            = aws_vpc.ljc_vpc_tf.id
+  cidr_block        = each.value[0]
+  availability_zone = each.value[1]
+  tags              = merge(local.common_tags, { Name = each.value[2] })
+}
+
+
+# Criação do Internet Gateway
+resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.ljc_vpc_tf.id
 
   tags = {
@@ -48,30 +45,55 @@ resource "aws_internet_gateway" "gw" {
   }
 }
 
-resource "aws_route_table" "public" {
+# Criação do Nat Gateway
+resource "aws_nat_gateway" "ngw" {
+connectivity_type = "private"
+for_each = local.subnet_privada_ids
+subnet_id         = each.value
+  tags = {
+    Name = "aws_nat_gateway_terraform"
+  }
+}
+
+# Criação route table acesso publico
+resource "aws_route_table" "rt_publica" {
   vpc_id = aws_vpc.ljc_vpc_tf.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
+    gateway_id = aws_internet_gateway.igw.id
   }
 
   tags = {
-    Name = "route_table_terraform"
+    Name = "route_table_terraform publica"
   }
 }
 
-resource "aws_route_table_association" "rtassoc_subnet_aza" {
-  subnet_id      = aws_subnet.ljc_subnet_a.id
-  route_table_id = aws_route_table.public.id
+
+# Criação route table acesso privado
+resource "aws_route_table" "rt_privada" {
+  vpc_id = aws_vpc.ljc_vpc_tf.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+#    gateway_id = aws_nat_gateway.ngw.id
+  }
+
+  tags = {
+    Name = "route_table_terraform privada"
+  }
 }
 
-resource "aws_route_table_association" "rtassoc_subnet_azb" {
-  subnet_id      = aws_subnet.ljc_subnet_b.id
-  route_table_id = aws_route_table.public.id
+# Associação das Subnets na Route Table publica
+resource "aws_route_table_association" "rtassoc_subnet_publica" {
+  for_each = local.subnet_publica_ids
+  subnet_id      = each.value
+  route_table_id = aws_route_table.rt_publica.id
 }
 
-resource "aws_route_table_association" "rtassoc_subnet_azc" {
-  subnet_id      = aws_subnet.ljc_subnet_c.id
-  route_table_id = aws_route_table.public.id
+# Associação das Subnets na Route Table privada
+resource "aws_route_table_association" "rtassoc_subnet_privada" {
+  for_each = local.subnet_privada_ids
+  subnet_id      = each.value
+  route_table_id = aws_route_table.rt_privada.id
 }
